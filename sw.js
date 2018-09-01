de9a42298ba7d5974cece417a51c9510955a805e
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-01 09:29:14 -0400

    working on caching the restaurants.json file

--- sw.js
+++ sw.js
@@ -8,6 +8,8 @@ const cacheFiles = [
     '/js/main.js',
     '/js/restaurant_info.js',
     '/data/restaurants.json',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
     '/img/1.jpg',
     '/img/2.jpg',
     '/img/3.jpg',
@@ -21,9 +23,9 @@ const cacheFiles = [
 ];
 
 self.addEventListener('install', function(e){
-    e.waitUntil
-        (caches.open(cacheName).then(function(cache){
-            return cache.addAll(cacheFiles);
+    e.waitUntil (
+        caches.open('cacheName').then(function(cache){
+            return cache.addAll(cacheFiles);                
         })
     );
 });
