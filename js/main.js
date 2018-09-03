16245867d4a708e2915999548e7674ead50cac90
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-03 12:45:27 -0400

    Added aria-label to View Details

--- js/main.js
+++ js/main.js
@@ -189,6 +189,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label','View Details' + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   more.tabIndex='3';
   li.append(more)
