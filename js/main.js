c386a8d2751ab6d1e3b738fb8c16a461c3b4d87a
Katarzyna Mazur <mazur66@hotmail.com>
2018-09-20 00:07:14 -0400

    Stored the map key in a variable

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1Ijoia2FzaWFtYXp1ciIsImEiOiJjamwzOWdvYmcwNWpzM3ZyeGQ1N2l0c2JjIn0.5M4pwqPO9bw-OJwatHh3TA',
+    mapboxToken = key,
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
